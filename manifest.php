3d08fcc9db6ad364551e2537cc4587be91e1be43
jonmlas <jonmlas@gmail.com>
2025-08-21 19:03:21 +0800

    Manifest updated

--- manifest.php
+++ manifest.php
@@ -1,26 +1,37 @@
 <?php if ( ! defined( 'FW' ) ) {
-	die( 'Forbidden' );
+    die( 'Forbidden' );
 }
 
 $manifest = array();
 
-$manifest['name']         = __( 'Sidebars', 'fw' );
-$manifest['description']  = __(
-	'Brings a new layer of customization freedom to your website by letting you add more than one sidebar to a page,'
-	.' or different sidebars on different pages.',
-	'fw'
+$manifest['name']        = __( 'Sidebars', 'fw' );
+$manifest['slug']        = 'unysonplus-sidebars';
+$manifest['description'] = __(
+    'Brings a new layer of customization freedom to your website by letting you add more than one sidebar to a page, or different sidebars on different pages.',
+    'fw'
 );
-$manifest['version']      = '1.0.20';
-$manifest['display']      = true;
-$manifest['standalone']   = true;
-$manifest['requirements'] = array(
-	'framework' => array(
-		'min_version' => '2.2.2', // in this version was added option handler
-	)
-);
-$manifest['github_repo'] = 'https://github.com/ThemeFuse/Unyson-Sidebars-Extension';
-$manifest['uri'] = 'http://manual.unyson.io/en/latest/extension/sidebars/index.html#content';
-$manifest['author'] = 'ThemeFuse';
-$manifest['author_uri'] = 'http://themefuse.com/';
 
-$manifest['github_update'] = 'ThemeFuse/Unyson-Sidebars-Extension';
+$manifest['version']     = '1.0.20';
+$manifest['display']     = true;
+$manifest['standalone']  = true;
+
+$manifest['requirements'] = array(
+    'framework' => array(
+        'min_version' => '2.2.2'
+    )
+);
+
+// Repo Info
+$manifest['github_update'] = 'UnysonPlus/UnysonPlus-Sidebars-Extension';
+$manifest['github_repo']   = 'https://github.com/UnysonPlus/UnysonPlus-Sidebars-Extension';
+$manifest['github_branch'] = 'master';
+
+// Author Info
+$manifest['author']     = 'UnysonPlus';
+$manifest['author_uri'] = 'https://www.lastimosa.com.ph/unysonplus';
+
+// Extra Meta
+$manifest['license']      = 'GPL-2.0-or-later';
+$manifest['text_domain']  = 'fw';
+$manifest['requires_php'] = '7.4';
+$manifest['requires_wp']  = '5.8';
