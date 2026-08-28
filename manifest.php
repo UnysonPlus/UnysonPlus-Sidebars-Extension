401a6e3b09a94e66f97cfa653e04e792c9f2829f
jonmlas <jonmlas@gmail.com>
2026-08-28 22:02:12 +0800

    Version bump

--- manifest.php
+++ manifest.php
@@ -11,7 +11,7 @@ $manifest['description'] = __(
     'fw'
 );
 
-$manifest['version']     = '1.0.29';
+$manifest['version']     = '1.0.30';
 $manifest['display']     = true;
 $manifest['standalone']  = true;
 
